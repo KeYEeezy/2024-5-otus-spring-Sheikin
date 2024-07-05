cf46b5d54251b1347ad6edea9006f71b7bcd9c7d
Nikita Sheikin <ksna12@yandex.ru>
2024-07-05 20:26:11 +0300

    Решение hw03

---
+++ hw03/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,45 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.utils.AbstractTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+public class TestServiceImplTest extends AbstractTest {
+    @InjectMocks
+    private TestServiceImpl testService;
+
+    @Mock
+    private LocalizedIOService ioService;
+
+    @Mock
+    private QuestionDao questionDao;
+
+    @Test
+    void executePositiveTest() {
+        when(questionDao.findAll()).thenReturn(csvQuestionDao.findAll());
+
+        when(ioService.readIntForRangeWithPromptLocalized(anyInt(), anyInt(), any(), any())).thenReturn(1);
+
+        var testResult = testService.executeTestFor(testStudent);
+
+        assertEquals(2, testResult.getRightAnswersCount());
+    }
+
+    @Test
+    void executeNegativeTest() {
+        when(questionDao.findAll()).thenReturn(csvQuestionDao.findAll());
+
+        when(ioService.readIntForRangeWithPromptLocalized(anyInt(), anyInt(), any(), any())).thenReturn(3);
+
+        var testResult = testService.executeTestFor(testStudent);
+
+        assertEquals(0, testResult.getRightAnswersCount());
+    }
+}
